207d1d6b5d3d781e203dd1d518bc5b0550d83ab7
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-01-02 20:27:42 -0600

    Change all the workflow stuff

--- custom.mk
+++ custom.mk
@@ -1,16 +1,17 @@
 WORKING_DIRECTORY := $(shell pwd)
 LOCAL_BIN := ${WORKING_DIRECTORY}/bin
 
-DEVOPS := ${LOCAL_BIN}/devops
+DEVCTL := ${LOCAL_BIN}/devctl
 
 export GOBIN := ${LOCAL_BIN}
 
-bin/devops: .versions/devops
-	go install github.com/unmango/go/cmd/devops@v$(shell $(DEVOPS) version devops)
+bin/devctl: .versions/devctl
+	go install github.com/unmango/devctl/cmd@v$(shell cat $<)
+	mv ${LOCAL_BIN}/cmd $@
 
 bin/kubebuilder: .versions/kubebuilder
-	go install sigs.k8s.io/kubebuilder/v4/cmd@v$(shell $(DEVOPS) version kubebuilder)
-	mv bin/cmd $@ && chmod +x $@
+	go install sigs.k8s.io/kubebuilder/v4/cmd@v$(shell cat $<)
+	mv ${LOCAL_BIN}/cmd $@ && chmod +x $@
 
 .envrc: hack/example.envrc
 	cp $< $@
