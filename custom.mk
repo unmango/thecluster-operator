03c09f886610bbfd1f14a5c6988e88a2f5eaf567
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-04-07 20:57:59 -0500

    Update stuff

--- custom.mk
+++ custom.mk
@@ -2,7 +2,7 @@ WORKING_DIRECTORY := $(shell pwd)
 LOCAL_BIN := ${WORKING_DIRECTORY}/bin
 
 include .versions/*.mk
-DEVCTL := ${LOCAL_BIN}/devctl
+DEVCTL := go tool devctl
 
 export GOBIN := ${LOCAL_BIN}
 
@@ -10,10 +10,6 @@ tidy: go.sum
 
 go.sum:
 	go mod tidy
-
-bin/devctl: .versions/devctl
-	go install github.com/unmango/devctl/cmd@v$(shell cat $<)
-	mv ${LOCAL_BIN}/cmd $@
 
 bin/kubebuilder: .versions/kubebuilder
 	go install sigs.k8s.io/kubebuilder/v4/cmd@$(shell cat $<)
