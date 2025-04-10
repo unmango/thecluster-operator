e9ff6b4ac433f33f78352921ddc9644114dfc01a
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-04-09 20:49:32 -0500

    Make the other things tools too (#29)

--- custom.mk
+++ custom.mk
@@ -2,22 +2,14 @@ WORKING_DIRECTORY := $(shell pwd)
 LOCAL_BIN := ${WORKING_DIRECTORY}/bin
 
 include .versions/*.mk
-DEVCTL := ${LOCAL_BIN}/devctl
 
-export GOBIN := ${LOCAL_BIN}
+DEVCTL      := go tool devctl
+KUBEBUILDER := go tool kubebuilder
 
 tidy: go.sum
 
-go.sum:
+go.sum: go.mod $(shell $(DEVCTL) list --go)
 	go mod tidy
-
-bin/devctl: .versions/devctl
-	go install github.com/unmango/devctl/cmd@v$(shell cat $<)
-	mv ${LOCAL_BIN}/cmd $@
-
-bin/kubebuilder: .versions/kubebuilder
-	go install sigs.k8s.io/kubebuilder/v4/cmd@$(shell cat $<)
-	mv ${LOCAL_BIN}/cmd $@ && chmod +x $@
 
 .envrc: hack/example.envrc
 	cp $< $@
