61d849517bfc79c4f149caa5e668c2b3cf72e971
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-04-20 11:23:26 -0500

    Tests for config map selectors

--- internal/controller/pia/wireguardconfig_controller_test.go
+++ internal/controller/pia/wireguardconfig_controller_test.go
@@ -417,6 +417,254 @@ var _ = Describe("WireguardConfig Controller", func() {
 			})
 		})
 
+		When("username is provided in a config map", func() {
+			const usernameKey = "pia-username"
+
+			configMapName := types.NamespacedName{
+				Name:      "my-credentials",
+				Namespace: typeNamespacedName.Namespace,
+			}
+
+			BeforeEach(func(ctx context.Context) {
+				By("Creating the config map")
+				cm := &corev1.ConfigMap{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      configMapName.Name,
+						Namespace: configMapName.Namespace,
+					},
+					Data: map[string]string{
+						usernameKey: piaUser,
+					},
+				}
+				Expect(k8sClient.Create(ctx, cm)).To(Succeed())
+
+				wireguardconfig.Spec.Username = piav1alpha1.WireguardClientConfigValue{
+					ConfigMapKeyRef: &corev1.ConfigMapKeySelector{
+						LocalObjectReference: corev1.LocalObjectReference{
+							Name: cm.Name,
+						},
+						Key: usernameKey,
+					},
+				}
+			})
+
+			AfterEach(func(ctx context.Context) {
+				By("Cleaning up the config map")
+				cm := &corev1.ConfigMap{}
+				if err := k8sClient.Get(ctx, configMapName, cm); err == nil {
+					Expect(k8sClient.Delete(ctx, cm)).To(Succeed())
+				}
+			})
+
+			It("should create a job to generate the config", func() {
+				By("Reconciling the created resource")
+				controllerReconciler := &WireguardConfigReconciler{
+					Client: k8sClient,
+					Scheme: k8sClient.Scheme(),
+				}
+
+				_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+					NamespacedName: typeNamespacedName,
+				})
+				Expect(err).NotTo(HaveOccurred())
+
+				By("Fetching the config resource")
+				resource := &piav1alpha1.WireguardConfig{}
+				err = k8sClient.Get(ctx, typeNamespacedName, resource)
+				Expect(err).NotTo(HaveOccurred())
+
+				generating := meta.IsStatusConditionTrue(
+					resource.Status.Conditions,
+					TypeGeneratingWireguardConfig,
+				)
+				Expect(generating).To(BeTrueBecause("The config is generating"))
+
+				podName := types.NamespacedName{
+					Namespace: typeNamespacedName.Namespace,
+					Name:      "generate-config",
+				}
+				pod := &corev1.Pod{}
+				Eventually(func() error {
+					return k8sClient.Get(ctx, podName, pod)
+				}).Should(Succeed())
+
+				Expect(pod.OwnerReferences).To(ConsistOf(And(
+					HaveField("Kind", "WireguardConfig"),
+					HaveField("Name", resourceName),
+				)))
+
+				Expect(pod.Spec.Volumes).To(ConsistOf(
+					corev1.Volume{
+						Name: "results",
+						VolumeSource: corev1.VolumeSource{
+							EmptyDir: &corev1.EmptyDirVolumeSource{},
+						},
+					},
+				))
+
+				Expect(pod.Spec.Containers).To(ConsistOf(
+					And(
+						HaveField("Name", "generate-config"),
+						HaveField("Image", "unstoppablemango/pia-manual-connections:v0.2.0-pia2023-02-06r0"),
+						HaveField("Env", []corev1.EnvVar{
+							{
+								Name: "PIA_USER",
+								ValueFrom: &corev1.EnvVarSource{
+									ConfigMapKeyRef: resource.Spec.Username.ConfigMapKeyRef,
+								},
+							},
+							{Name: "PIA_PASS", Value: piaPass},
+							{Name: "PIA_PF", Value: "false"},
+							{Name: "PIA_CONNECT", Value: "false"},
+							{Name: "PIA_CONF_PATH", Value: "/out/pia0.conf"},
+							{Name: "VPN_PROTOCOL", Value: "wireguard"},
+							{Name: "DISABLE_IPV6", Value: "no"},
+							{Name: "DIP_TOKEN", Value: "no"},
+							{Name: "AUTOCONNECT", Value: "true"},
+						}),
+						HaveField("VolumeMounts", []corev1.VolumeMount{{
+							Name:      "results",
+							MountPath: "/out",
+						}}),
+					),
+					And(
+						HaveField("Name", "results"),
+						HaveField("Image", "busybox:latest"),
+						HaveField("Command", []string{"sh", "-c", "sleep infinity"}),
+						HaveField("VolumeMounts", []corev1.VolumeMount{{
+							Name:      "results",
+							ReadOnly:  true,
+							MountPath: "/out",
+						}}),
+					),
+				))
+			})
+		})
+
+		When("password is provided in a config map", func() {
+			const passwordKey = "pia-password"
+
+			configMapName := types.NamespacedName{
+				Name:      "my-credentials",
+				Namespace: typeNamespacedName.Namespace,
+			}
+
+			BeforeEach(func(ctx context.Context) {
+				By("Creating the config map")
+				cm := &corev1.ConfigMap{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      configMapName.Name,
+						Namespace: configMapName.Namespace,
+					},
+					Data: map[string]string{
+						passwordKey: piaPass,
+					},
+				}
+				Expect(k8sClient.Create(ctx, cm)).To(Succeed())
+
+				wireguardconfig.Spec.Password = piav1alpha1.WireguardClientConfigValue{
+					ConfigMapKeyRef: &corev1.ConfigMapKeySelector{
+						LocalObjectReference: corev1.LocalObjectReference{
+							Name: cm.Name,
+						},
+						Key: passwordKey,
+					},
+				}
+			})
+
+			AfterEach(func(ctx context.Context) {
+				By("Cleaning up the config map")
+				cm := &corev1.ConfigMap{}
+				if err := k8sClient.Get(ctx, configMapName, cm); err == nil {
+					Expect(k8sClient.Delete(ctx, cm)).To(Succeed())
+				}
+			})
+
+			It("should create a job to generate the config", func() {
+				By("Reconciling the created resource")
+				controllerReconciler := &WireguardConfigReconciler{
+					Client: k8sClient,
+					Scheme: k8sClient.Scheme(),
+				}
+
+				_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+					NamespacedName: typeNamespacedName,
+				})
+				Expect(err).NotTo(HaveOccurred())
+
+				By("Fetching the config resource")
+				resource := &piav1alpha1.WireguardConfig{}
+				err = k8sClient.Get(ctx, typeNamespacedName, resource)
+				Expect(err).NotTo(HaveOccurred())
+
+				generating := meta.IsStatusConditionTrue(
+					resource.Status.Conditions,
+					TypeGeneratingWireguardConfig,
+				)
+				Expect(generating).To(BeTrueBecause("The config is generating"))
+
+				podName := types.NamespacedName{
+					Namespace: typeNamespacedName.Namespace,
+					Name:      "generate-config",
+				}
+				pod := &corev1.Pod{}
+				Eventually(func() error {
+					return k8sClient.Get(ctx, podName, pod)
+				}).Should(Succeed())
+
+				Expect(pod.OwnerReferences).To(ConsistOf(And(
+					HaveField("Kind", "WireguardConfig"),
+					HaveField("Name", resourceName),
+				)))
+
+				Expect(pod.Spec.Volumes).To(ConsistOf(
+					corev1.Volume{
+						Name: "results",
+						VolumeSource: corev1.VolumeSource{
+							EmptyDir: &corev1.EmptyDirVolumeSource{},
+						},
+					},
+				))
+
+				Expect(pod.Spec.Containers).To(ConsistOf(
+					And(
+						HaveField("Name", "generate-config"),
+						HaveField("Image", "unstoppablemango/pia-manual-connections:v0.2.0-pia2023-02-06r0"),
+						HaveField("Env", []corev1.EnvVar{
+							{Name: "PIA_USER", Value: piaUser},
+							{
+								Name: "PIA_PASS",
+								ValueFrom: &corev1.EnvVarSource{
+									ConfigMapKeyRef: resource.Spec.Password.ConfigMapKeyRef,
+								},
+							},
+							{Name: "PIA_PF", Value: "false"},
+							{Name: "PIA_CONNECT", Value: "false"},
+							{Name: "PIA_CONF_PATH", Value: "/out/pia0.conf"},
+							{Name: "VPN_PROTOCOL", Value: "wireguard"},
+							{Name: "DISABLE_IPV6", Value: "no"},
+							{Name: "DIP_TOKEN", Value: "no"},
+							{Name: "AUTOCONNECT", Value: "true"},
+						}),
+						HaveField("VolumeMounts", []corev1.VolumeMount{{
+							Name:      "results",
+							MountPath: "/out",
+						}}),
+					),
+					And(
+						HaveField("Name", "results"),
+						HaveField("Image", "busybox:latest"),
+						HaveField("Command", []string{"sh", "-c", "sleep infinity"}),
+						HaveField("VolumeMounts", []corev1.VolumeMount{{
+							Name:      "results",
+							ReadOnly:  true,
+							MountPath: "/out",
+						}}),
+					),
+				))
+			})
+		})
+
 		When("a matching config exists", func() {
 			BeforeEach(func(ctx context.Context) {
 				By("Creating a matching config map")
