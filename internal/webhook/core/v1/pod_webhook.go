42fe7a1c5f04708fb9f0adead4f910fca955a19e
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-04-23 22:34:46 -0500

    Kink working out

--- internal/webhook/core/v1/pod_webhook.go
+++ internal/webhook/core/v1/pod_webhook.go
@@ -20,10 +20,14 @@ import (
 	"context"
 	"fmt"
 
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
+
+	piav1alpha1 "github.com/unmango/thecluster-operator/api/pia/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
 )
 
 // nolint:unused
@@ -33,13 +37,17 @@ var podlog = logf.Log.WithName("pod-resource")
 // SetupPodWebhookWithManager registers the webhook for Pod in the manager.
 func SetupPodWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).For(&corev1.Pod{}).
-		WithDefaulter(&PodCustomDefaulter{}).
+		WithDefaulter(&PodCustomDefaulter{
+			Client: mgr.GetClient(),
+		}).
 		Complete()
 }
 
 // +kubebuilder:webhook:path=/mutate--v1-pod,mutating=true,failurePolicy=fail,sideEffects=None,groups="",resources=pods,verbs=create;update,versions=v1,name=mpod-v1.kb.io,admissionReviewVersions=v1
 
-type PodCustomDefaulter struct{}
+type PodCustomDefaulter struct {
+	client.Client
+}
 
 // Default implements webhook.CustomDefaulter so a webhook will be registered for the Kind Pod.
 func (d *PodCustomDefaulter) Default(ctx context.Context, obj runtime.Object) error {
@@ -47,9 +55,27 @@ func (d *PodCustomDefaulter) Default(ctx context.Context, obj runtime.Object) er
 	if !ok {
 		return fmt.Errorf("expected an Pod object but got %T", obj)
 	}
-	podlog.Info("Defaulting for Pod", "name", pod.GetName())
 
-	// TODO(user): fill in your defaulting logic.
+	var configName string
+	if configName, ok = pod.Annotations["pia.thecluster.io/config"]; !ok {
+		podlog.Info("Ignoring Pod without matching annotation", "name", pod.GetName())
+		return nil
+	}
+
+	podlog.Info("Applying config init container to Pod", "name", pod.GetName())
+
+	config := &piav1alpha1.WireguardConfig{}
+	if err := d.Get(ctx, types.NamespacedName{
+		Namespace: pod.Namespace,
+		Name:      configName,
+	}, config); err != nil {
+		return fmt.Errorf("unable to look up wireguard config: %w", err)
+	}
+
+	container := corev1.Container{
+		Name: "generate-wireguard-config",
+	}
+	pod.Spec.InitContainers = append(pod.Spec.InitContainers, container)
 
 	return nil
 }
